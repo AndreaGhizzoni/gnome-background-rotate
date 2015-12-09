4684718ea9f8e6de69690cc68f52127e74b00b48
AndreaGhizzoni <andrea.ghz@gmail.com>
2015-12-09 21:56:27 +0100

    change gsettings option from spanned to centered to support multimple monitor image

--- rotate.sh
+++ rotate.sh
@@ -16,6 +16,6 @@ wallpaper=`find "${dir}" -type f | shuf -n1`
 
 # Change wallpaper.
 # http://bit.ly/HYEU9H
-gsettings set org.gnome.desktop.background picture-options "spanned"
+gsettings set org.gnome.desktop.background picture-options "centered"
 gsettings set org.gnome.desktop.background picture-uri "file://${wallpaper}"
 
